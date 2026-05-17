30e25850465c481558b974600ea9522c4cc5190a
milan <155627437+milan087117@users.noreply.github.com>
2026-05-17 16:17:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const barcelonaTotal = 25 + 100 + 120;
+const madridTotal = 30 + 120 + 140;
+const sevilleTotal = 20 + 80 + 130;
+
+document.getElementById("barcelona-total").innerHTML = barcelonaTotal + " €";
+document.getElementById("madrid-total").innerHTML = madridTotal + " €";
+document.getElementById("seville-total").innerHTML = sevilleTotal + " €";
